32575d345b6ac2f12111d68144989c3fe4692636
senna-lang <asrsena3302@gmail.com>
2023-12-19 21:26:31 +0900

    テキスト詳細ページ作成

---
+++ src/app/detailtexts/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+import { collection, doc,query } from 'firebase/firestore';
+import { db } from '@/app/lib/firebase';
+
+const DetailTexts = () => {
+ 
+
+
+  return <div>DetailTexts</div>;
+};
+
+export default DetailTexts;

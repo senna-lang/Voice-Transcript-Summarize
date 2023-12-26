3f856f6547755c2ac04c0b71c98023f2729d779a
senna-lang <asrsena3302@gmail.com>
2023-12-26 23:52:29 +0900

    UI調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
-'use client'
+'use client';
 import React from 'react';
 import Form from './components/Form';
 import Link from 'next/link';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 const Home = () => {
   return (
     <div>
-      <Form />
-      <Link href='/profile'>ユーザー情報</Link>
+      {/* <Form />
+      <Link href='/profile'>ユーザー情報</Link> */}
+      <div className="flex h-screen justify-center items-center bg-dark-mode">
+        <div className="h-full flex" style={{ width: '1280px' }}>
+          <div className="w-1/5 h-full border-r"><Sidebar /></div>
+          <div className="w-4/5 h-full"><Chat /></div>
+        </div>
+      </div>
     </div>
   );
 };

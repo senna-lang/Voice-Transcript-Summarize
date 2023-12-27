eb67bfffb3269992d5ab8b0399dab5f8d3a788a5
senna-lang <asrsena3302@gmail.com>
2023-12-27 23:18:21 +0900

    小さな修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import React from 'react';
-import Form from './components/Form';
-import Link from 'next/link';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
 
@@ -10,12 +8,14 @@ export const runtime = 'edge';
 const Home = () => {
   return (
     <div>
-      {/* <Form />
-      <Link href='/profile'>ユーザー情報</Link> */}
       <div className="flex h-screen justify-center items-center bg-dark-mode">
         <div className="h-full flex" style={{ width: '1280px' }}>
-          <div className="w-1/5 h-full border-r"><Sidebar /></div>
-          <div className="w-4/5 h-full"><Chat /></div>
+          <div className="w-1/5 h-full border-r">
+            <Sidebar />
+          </div>
+          <div className="w-4/5 h-full">
+            <Chat />
+          </div>
         </div>
       </div>
     </div>

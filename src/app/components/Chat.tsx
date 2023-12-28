5a697416389cf65ecf6829007982f189fc33b6a7
senna-lang <asrsena3302@gmail.com>
2023-12-28 13:03:56 +0900

    ドキュメント削除機能実装

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -5,17 +5,47 @@ import { textIdState } from '../atoms/textId';
 import { textTitleState } from '../atoms/textTitle';
 import { TextDetail } from '../types/types';
 import { useTextDetail } from '../hooks/useTextDetail';
+import { Button } from '@mantine/core';
+import { collection, deleteDoc, doc, getDocs, query } from 'firebase/firestore';
+import { db } from '../lib/firebase';
+import { useTextMeta } from '../hooks/useTextMeta';
 
 const Chat = () => {
   const [userId] = useRecoilState(userIdState);
-  const [textId] = useRecoilState(textIdState);
-  const [textTitle] = useRecoilState(textTitleState);
+  const [textId, setTextId] = useRecoilState(textIdState);
+  const [textTitle, setTitle] = useRecoilState(textTitleState);
   const { textDetail, isMutating } = useTextDetail(textId);
+  const { metaTrigger } = useTextMeta(userId);
+
+  const handleDelete = async (textId: string) => {
+    const docRef = doc(db, 'texts', textId);
+    await deleteDoc(docRef);
+    const subCollectionRef = collection(docRef, 'text');
+    const subDocsQuery = query(subCollectionRef);
+    const subDocsSnapshot = await getDocs(subDocsQuery);
+    subDocsSnapshot.forEach(async subDoc => {
+      const subDocRef = doc(subCollectionRef, subDoc.id);
+      await deleteDoc(subDocRef);
+    });
+    metaTrigger();
+    setTitle('');
+    setTextId('');
+  };
+
   return (
     <div className=" h-full p-4 flex flex-col">
-      <h1 className="text-2xl text-white font-semibold mb-4">
-        {userId ? textTitle : <></>}
-      </h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl text-white font-semibold mb-4">
+          {userId ? textTitle : <></>}
+        </h1>
+        {textTitle == '' ? (
+          <></>
+        ) : (
+          <Button className="mb-4" onClick={() => handleDelete(textId)}>
+            削除
+          </Button>
+        )}
+      </div>
       <div className="flex-grow overflow-y-auto mb-4">
         {userId && textDetail ? (
           textDetail.map((textDetail: TextDetail) => (

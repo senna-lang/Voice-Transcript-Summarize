b36c786b6303161abc4d45afecffd836e7bea04a
senna-lang <asrsena3302@gmail.com>
2024-01-01 22:18:22 +0900

    gptへの要約リクエスト用API実装

---
+++ src/app/api/chatgpt/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+export async function POST(req: NextRequest) {
+  const openAi = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+  const { prompt } = await req.json();
+  const gptRes = await openAi.chat.completions.create({
+    messages: [{ role: 'user', content: prompt }],
+    model: 'gpt-3.5-turbo',
+  });
+  const response = NextResponse.json(gptRes);
+  return response;
+}

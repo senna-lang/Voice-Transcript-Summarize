06ad625613842e9f7fe9ae8392b87d46f9cc55c9
senna-lang <asrsena3302@gmail.com>
2024-01-04 17:18:08 +0900

    削除確認モーダル実装、その他UI変更

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -5,10 +5,12 @@ import { textIdState } from '../atoms/textId';
 import { textTitleState } from '../atoms/textTitle';
 import { TextDetail } from '../types/types';
 import { useTextDetail } from '../hooks/useTextDetail';
-import { Button } from '@mantine/core';
+import { Button, Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { collection, deleteDoc, doc, getDocs, query } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 import { useTextMeta } from '../hooks/useTextMeta';
+import { FaRegTrashAlt } from 'react-icons/fa';
 
 const Chat = () => {
   const [userId] = useRecoilState(userIdState);
@@ -16,6 +18,7 @@ const Chat = () => {
   const [textTitle, setTitle] = useRecoilState(textTitleState);
   const { textDetail, isMutating } = useTextDetail(textId);
   const { metaTrigger } = useTextMeta(userId);
+  const [opened, { open, close }] = useDisclosure(false);
 
   const handleDelete = async (textId: string) => {
     const docRef = doc(db, 'texts', textId);
@@ -30,33 +33,75 @@ const Chat = () => {
     metaTrigger();
     setTitle('');
     setTextId('');
+    close;
   };
 
   return (
-    <div className=" h-full p-4 flex flex-col">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl text-white font-semibold mb-4">
-          {userId ? textTitle : <></>}
-        </h1>
-        {textTitle == '' ? (
-          <></>
-        ) : (
-          <Button className="mb-4" onClick={() => handleDelete(textId)}>
-            削除
-          </Button>
-        )}
-      </div>
-      <div className="flex-grow overflow-y-auto mb-4">
-        {userId && textDetail ? (
-          textDetail.map((textDetail: TextDetail) => (
-            <div key={textDetail.summary}>
-              <div className="text-white">{textDetail.vanilla}</div>
-              <div className="text-white">{textDetail.summary}</div>
+    <div className="p-6 flex flex-col bg-slate-100 h-[93vh]">
+      <div className=" min-h-full bg-white p-5 rounded-lg overflow-y-auto">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl font-bold ">{userId ? textTitle : <></>}</h1>
+          {textTitle == '' ? (
+            <></>
+          ) : (
+            <div
+              className="font-semibold text-xl text-blue-500 inline-block cursor-pointer "
+              onClick={open}
+            >
+              <FaRegTrashAlt />
             </div>
-          ))
-        ) : (
-          <></>
-        )}
+          )}
+        </div>
+        <Modal opened={opened} onClose={close} centered withCloseButton={false}>
+          <div className="p-4">
+            <h1 className=" w-full text-lg text-left mb-1">
+              本当に削除してもいいですか？
+            </h1>
+            <p className=" text-slate-600  mb-10">
+              一度削除すると復元できません
+            </p>
+            <div className="flex justify-end">
+              <Button onClick={close} className="mr-2" variant='outline' >
+                キャンセル
+              </Button>
+              <Button
+                onClick={() => {
+                  handleDelete(textId);
+                  close();
+                }}
+                color="red"
+              >
+                削除
+              </Button>
+            </div>
+          </div>
+        </Modal>
+        <div className="flex-grow mb-4">
+          {userId && textDetail ? (
+            textDetail.map((textDetail: TextDetail) => (
+              <div key={textDetail.summary}>
+                <div className="mb-5">
+                  <h2 className=" font-semibold text-2xl w-full text-center mb-2">
+                    Summary
+                  </h2>
+                  <div className=" border-dashed border-blue-300 border-2 p-6 rounded-lg">
+                    <div>{textDetail.summary}</div>
+                  </div>
+                </div>
+                <div>
+                  <h2 className=" font-semibold text-2xl w-full text-center mb-2">
+                    Transcript
+                  </h2>
+                  <div className="border-dashed border-blue-300 border-2 p-6 rounded-lg">
+                    <div>{textDetail.vanilla}</div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );

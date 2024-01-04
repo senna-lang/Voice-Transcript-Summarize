aff25d6409dd60776761a599c7a1a50ee36fd142
senna-lang <asrsena3302@gmail.com>
2024-01-04 17:17:05 +0900

    headerUI追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import Header from './components/Header';
 
 export const runtime = 'edge';
 
 const Home = () => {
   return (
     <div>
-      <div className="flex h-screen justify-center items-center bg-dark-mode">
+      <div className="flex h-screen justify-center items-center bg-slate-300">
         <div className="h-full flex" style={{ width: '1280px' }}>
-          <div className="w-1/5 h-full border-r">
+          <div className="w-1/4 h-full border-r">
             <Sidebar />
           </div>
-          <div className="w-4/5 h-full">
+          <div className="w-3/4 max-h-screen">
+            <Header/>
             <Chat />
           </div>
         </div>

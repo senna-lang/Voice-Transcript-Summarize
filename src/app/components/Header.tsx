9863dd42bb7e1c8ef5e43d0f80bbe6eece102813
senna-lang <asrsena3302@gmail.com>
2024-01-05 14:58:26 +0900

    ダッシュボード表示をuser.emailに変更

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { PasswordInput} from '@mantine/core';
-import { useUserIdState } from '@/app/atoms/userId';
+import { useUserState } from '../atoms/user';
+
 
 const Header = () => {
-  const [userId] = useUserIdState();
+  const [user] = useUserState();
+
   return (
     <div className=" flex justify-between bg-white py-4 px-8 items-center h-[7vh]">
       <div className=' w-3/5'>
         <PasswordInput placeholder=" your OPENAI_API_KEY"/>
       </div>
       <div>
-         <p>{userId}</p>
+         <p>{user}</p>
       </div>
     </div>
   );

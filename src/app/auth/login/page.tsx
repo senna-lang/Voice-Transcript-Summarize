e9195f083e4734a395a339748f5eac27aeb5ee26
senna-lang <asrsena3302@gmail.com>
2024-01-05 14:57:10 +0900

    userIdの永続化

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -46,7 +46,7 @@ const Login = () => {
             )
               .then(userCredential => {
                 const unsubscribed = onAuthStateChanged(auth, newUser => {
-                  // setUser(newUser)
+                  setUser(newUser?.email ?? null);
                   setUserId(newUser?.uid ?? null);
                   router.push('/');
                 });
@@ -60,7 +60,7 @@ const Login = () => {
                 } else {
                   alert(error.message);
                 }
-              })
+              });
           })}
         >
           <TextInput

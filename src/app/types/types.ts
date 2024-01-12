039c4250956614d6eccdf4f895f2d31dc99ad70e
senna-lang <asrsena3302@gmail.com>
2024-01-12 17:10:32 +0900

    add Props type

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,15 +1,24 @@
-import { Timestamp } from "firebase/firestore";
-import { type } from "os";
+import { FieldValue, Timestamp } from 'firebase/firestore';
 
 type TextMeta = {
   id: string;
-  userId:string;
-  name:string;
+  userId: string;
+  name: string;
   createdAt: Timestamp;
+};
+type TextMetaProps = {
+  userId: string;
+  name: string;
+  createdAt: FieldValue;
 };
 
 type TextDetail = {
-  summary:string;
-  vanilla:string;
-}
-export type { TextMeta,TextDetail };
+  summary: string;
+  vanilla: string;
+};
+type TextDetailProps = {
+  summary: string | null;
+  vanilla: string;
+};
+
+export type { TextMeta, TextDetail, TextMetaProps, TextDetailProps };
